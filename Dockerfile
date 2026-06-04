b1cc6cd371077a660ca00bc98764f57ebaf9f013
Ariful <arifulmit17@gmail.com>
2026-06-04 19:23:36 +0600

    added git ignore

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM node:20-alpine
 
 WORKDIR /app
-ENV NODE_ENV=production
+ARG APP_ENV=development
+ENV NODE_ENV=$APP_ENV
 COPY package*.json ./
 RUN npm ci --only=production
 COPY . .
--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const NODE_ENV = process.env.NODE_ENV ;
+
 app.get('/', (req, res) => {
-  res.send('Hello, World from docker!');
+  res.send(`Hello, World from ${NODE_ENV}!`);
 });
 
 app.listen(PORT, () => {
